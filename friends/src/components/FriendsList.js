1b83aec0aa867499566f7208d8ce6b81c5e76a4d
Tate Keller <tatek1993@gmail.com>
2020-02-26 10:14:47 -0600

    Styled FriendsList.js and FriendsForm.js using react-strap and used styled-components to fix spacing and color issues

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -6,6 +6,29 @@ import styled from 'styled-components/macro';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import FriendsForm from './FriendsForm';
 
+const PaddedDiv = styled.div `
+    margin: 2%;
+    width: 20%;
+    background-color: #474647;
+    border-radius: 10px;
+    color: white;
+    border: 5px dotted red;
+    text-align: center
+`
+
+const CardHeaderStyle = styled.div `
+    background-color: #474647;
+    color: white;
+    font-size: 1.5rem;
+    padding: 1%;
+    border-bottom: 3px dotted #EEB902;
+
+`
+const CardTextStyle = styled.div `
+    border-bottom: 3px dotted #2D7DD2;
+    padding-bottom: 10px;
+     margin-bottom: 25px;
+`
 
 const FriendsList = () => {
 
@@ -24,13 +47,13 @@ const FriendsList = () => {
         <Container className="friends-list">
             <Row>
             {friends.map(friend => (
-                <Card className="friend-container" key={friend.id}>
-                    <CardHeader>{friend.name}</CardHeader>
+                <PaddedDiv className="friend-container" key={friend.id}>
+                    <CardHeaderStyle>{friend.name}</CardHeaderStyle>
                     <CardBody>
-                        <CardTitle>{friend.age}</CardTitle>
+                        <CardTextStyle>{friend.age}</CardTextStyle>
                         <CardText>{friend.email}</CardText>
                     </CardBody>
-                </Card>  
+                </PaddedDiv>  
             ))}
             </Row>
             <Row>
